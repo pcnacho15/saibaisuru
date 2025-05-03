2a650646d584dd58b09324dcad5a1e25d2ba132c
Cristian Martinez <cristiancamilomartinezs@gmail.com>
2025-05-03 11:16:33 -0500

    CallToAction

--- src/components/ui/cta/Cta.tsx
+++ src/components/ui/cta/Cta.tsx
@@ -8,27 +8,30 @@ export const Cta = () => {
     // Navigate to the seeds category page
     router.push("/categories/semillas");
 
-  }
+  } 
 
   return (
-    <section className="py-12 px-6 bg-[#581c87] text-white text-center rounded-xl shadow-lg mt-8">
-      <h2 className="text-3xl font-bold">
-        ¿Listo para comenzar tu propio cultivo?
-      </h2>
-      <p className="my-4">
-        Elige tus semillas, recibe asesoría y haz parte del mundo del
-        autocultivo.
-      </p>
-      <div className="flex gap-2 justify-center flex-wrap mb-4 text-sm text-gray-300">
-        <span>#20SonLegales</span>
-        <span>#Cultura420</span>
+    <section className="relative py-12 px-6  bg-[url(/imgs/banner-cta.png)] bg-cover bg-bottom text-white text-center rounded-xl shadow-lg mt-8">
+      <div className="block absolute rounded-xl inset-0 bg-black bg-opacity-45"></div>
+      <div className="relative z-10">
+        <h2 className="text-3xl font-bold">
+          ¿Listo para comenzar tu propio cultivo?
+        </h2>
+        <p className="my-4">
+          Elige tus semillas, recibe asesoría y haz parte del mundo del
+          autocultivo.
+        </p>
+        <div className="flex gap-2 justify-center flex-wrap mb-4 text-sm text-gray-300">
+          <span>#20SonLegales</span>
+          <span>#Cultura420</span>
+        </div>
+        <button
+          onClick={handlleNavigation}
+          className="px-6 py-2 rounded-xl bg-white text-[#581c87] font-bold hover:bg-gray-100 duration-200 active:scale-95 active:shadow-none transition-all"
+        >
+          Ir a la tienda
+        </button>
       </div>
-      <button
-        onClick={handlleNavigation}
-        className="px-6 py-2 rounded-xl bg-white text-[#581c87] font-bold hover:bg-gray-100 duration-200 active:scale-95 active:shadow-none transition-all"
-      >
-        Ir a la tienda
-      </button>
     </section>
   );
 };
